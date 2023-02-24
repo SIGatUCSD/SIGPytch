6ab0de9e4bb26dccc7a6ebebf38ebf1e62fa0639
pranay-jha <63332027+pranay-jha@users.noreply.github.com>
2023-02-24 11:13:07 -0800

    FEAT: Rolling Sharpe ratio metric and data class for periods

--- sigpytch/periods.py
+++ sigpytch/periods.py
@@ -29,31 +29,13 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
-import numpy as np
-import pandas as pd
+from dataclasses import dataclass
 
-from periods import P
-
-def rolling_sharpe(daily_returns: pd.Series,
-                   risk_free_rate: pd.Series, window: int) -> pd.Series:
+@dataclass
+class P:
     """
-    Computes the rolling Sharpe ratio for a given ticker and risk-free rate.
-    
-    Parameters:
-        daily_returns (pd.Series): Daily returns of the given ticker 
-        risk_free_rate (pd.Series): Risk-free rate (treasury yield)
-        window (int): Window size in months
-
-    Returns:
-        pd.Series: Rolling Sharpe ratio  
+    Class contains named constants for different period lengths 
     """
-    # Compute D-bar
-    avg_return = daily_returns.rolling(window = window * P.TDAYS_PER_MONTH).mean()
-    periodized_avg_return = avg_return * window * P.TDAYS_PER_MONTH
-    avg_diff_return = periodized_avg_return - risk_free_rate
-    # Compute sigma
-    volatility = daily_returns.rolling(window = window * P.TDAYS_PER_MONTH).std()
-    periodized_volatility = volatility * np.sqrt(window * P.TDAYS_PER_MONTH)
-    # Compute rolling Sharpe ratio: D-bar / sigma
-    rolling_sharpe_ratio = (avg_diff_return / periodized_volatility).dropna()
-    return rolling_sharpe_ratio
+    TDAYS_PER_MONTH = 21
+    TDAYS_PER_YEAR = 252
+
